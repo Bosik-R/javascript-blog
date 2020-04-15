6269dcd71a3bfe12471ea7971d77c5b8bb3fd717
Tomasz Rosik <Bosik27@gmail.com>
2020-04-15 22:45:54 +0200

    Add JS searching by "href" attribute, adding class "active" to article

--- js/script.js
+++ js/script.js
@@ -16,8 +16,9 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
+  event.preventDefault();
   const clickedElement = this;
   console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
@@ -29,11 +30,19 @@ const titleClickHandler = function(event){
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('href: ' + articleSelector);
 
-  /* add class 'active' to the correct article */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('targetArticle: ', targetArticle);
+
+  /* [DONE] add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
